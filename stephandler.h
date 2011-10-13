ed48de6fab72bd3e0e58cfd00d43764f3dc8e8e5
Constantine Nosovsky <knosovskyi@gmail.com>
2011-10-14 02:40:13 +0300

    backend with timestamps and upper limit

--- stephandler.h
+++ stephandler.h
@@ -1,8 +1,13 @@
 #ifndef STEPSENSOR_H
 #define STEPSENSOR_H
 
+#define MAGNETIC_FIELD_EARTH_MAX 60.0f
+#define MAGNETIC_FIELD_EARTH_MIN 30.0f
+#define GRAVITY_EARTH 9.806650161743164f
+
 #include <QAccelerometerFilter>
 #include <QDebug>
+#include <QDateTime>
 #include <math.h>
 
 //#define RADIANS_TO_DEGREES 57.2957795
@@ -17,21 +22,24 @@ private:
     float mScale;
     float mLastValues[3*2];
     float mLastDirections[3*2];
-    float mLimit;
+    float mLowerLimit, mUpperLimit;
     float mLastDiff[3*2];
     int mLastMatch;
     float mLastExtremes[3*2][3*2];
+    qint64 timestamp;
 
 public:
-    StepHandler() : x(0), y(0), z(0), t(0) {
+    StepHandler() : timestamp(0) {
         int h = 480; // TODO: remove this constant
         mYOffset = h * 0.5f;
-        mScale = -(h * 0.5f * (1.0f / 10)); //SensorManager.MAGNETIC_FIELD_EARTH_MAX
-        mLimit = 10;
+        mScale = -(h * 0.5f * (1.2f / MAGNETIC_FIELD_EARTH_MAX));
+        mLowerLimit = 8;
+        mUpperLimit = 40;
         mLastMatch = -1;
     }
 
     bool filter(QAccelerometerReading *reading) {
+        qint64 newTimestamp = QDateTime::currentMSecsSinceEpoch();
         qreal values[3] = { reading->x(), reading->y(), reading->z() };
 
         qreal vSum = 0;
@@ -42,13 +50,16 @@ public:
         float v = vSum / 3;
 
         float direction = (v > mLastValues[k] ? 1 : (v < mLastValues[k] ? -1 : 0));
-        if (direction == -mLastDirections[k]) {
+        if (direction == -mLastDirections[k] && newTimestamp - timestamp > 250) {
+            timestamp = newTimestamp;
             // Direction changed
             int extType = (direction > 0 ? 0 : 1); // minumum or maximum?
             mLastExtremes[extType][k] = mLastValues[k];
             float diff = abs(mLastExtremes[extType][k] - mLastExtremes[1 - extType][k]);
 
-            if (diff > mLimit) {
+            if (diff > mLowerLimit && diff < mUpperLimit) {
+                qDebug() << diff;
+
                 bool isAlmostAsLargeAsPrevious = diff > (mLastDiff[k]*2/3);
                 bool isPreviousLargeEnough = mLastDiff[k] > (diff/3);
                 bool isNotContra = (mLastMatch != 1 - extType);
@@ -66,8 +77,6 @@ public:
         mLastDirections[k] = direction;
         mLastValues[k] = v;
 
-        //qDebug() << t++ << rx << ry << rz;
-
 //        qreal divider = sqrt(rx * rx + ry * ry + rz * rz);
 
 //        float lowPassFactor = 0.1;
@@ -77,22 +86,6 @@ public:
 //        x = (acos(rx / divider) * RADIANS_TO_DEGREES - 90 - x) * lowPassFactor;
 //        y = (acos(ry / divider) * RADIANS_TO_DEGREES - 90 - y) * lowPassFactor;
 //        z = (acos(rz / divider) * RADIANS_TO_DEGREES - 90 - z) * lowPassFactor;
-
-//        //emit rotationChanged(x);
-
-//        qDebug() << t++ << x << y << z;
-
-//        qreal dx = x - rx;
-//        qreal dy = y - ry;
-//        qreal dz = z - rz;
-//        if(abs(rx) + abs(ry) + abs(rz) > 12
-//                )
-//        //if(abs(x - rx) > 1 && abs(y - ry) > 1 && abs(z - rz) > 1)
-//            emit onStep();
-
-//        x = rx;
-//        y = ry;
-//        z = rz;
 
         // Don't store the reading in the sensor.
         return false;
